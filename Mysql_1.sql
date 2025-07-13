b7aa1bf5074413772610e6c24005c1230c60e2a3
Legato <xizhang.lee@gmail.com>
2025-07-13 10:18:41 +0800

    Update Mysql_1.sql

--- Mysql_1.sql
+++ Mysql_1.sql
@@ -1,16 +1,16 @@
 /*
-#建立資料庫（如果尚未建立）
+#1. 建立資料庫（如果尚未建立）
 CREATE DATABASE IF NOT EXISTS testdb;
 USE testdb;
 
-#建立表格
+#2. 建立表格
 CREATE TABLE products (
     product_id INT PRIMARY KEY,
     low_fats CHAR(1) NOT NULL,
     recyclable CHAR(1) NOT NULL
 );
 
-#插入資料
+#3. 插入資料
 INSERT INTO products (product_id, low_fats, recyclable) VALUES
 (0, 'Y', 'N'),
 (1, 'Y', 'Y'),
@@ -21,7 +21,7 @@ INSERT INTO products (product_id, low_fats, recyclable) VALUES
 select * from products;
 
 #https://leetcode.cn/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=sql-free-50
-#[篩選資料]可回收且低脂的產品：篩選出low_fats 和 recyclable 都是"Y"的row
+#4. [篩選資料]可回收且低脂的產品：篩選出low_fats 和 recyclable 都是"Y"的row
 
 select product_id 
 from products
