76bccc356e99c66cef5db70afba20aa06c8b9238
Legato <xizhang.lee@gmail.com>
2025-07-14 15:30:43 +0800

    Update Mysql_5.sql

--- Mysql_5.sql
+++ Mysql_5.sql
@@ -4,7 +4,7 @@ CREATE TABLE if not exists tweets (
     content VARCHAR(100) NOT NULL
 );
 
-#當資料表有設定「唯一鍵」或「主鍵」時，可用以下方法規定，剛有一樣的值時則不insert
+#當資料表有設定「唯一鍵」或「主鍵」時，可用以下方法規定，當有一樣的值時則不insert
 #如果 tweet_id 已存在，就會忽略該筆資料、不插入也不報錯。
 INSERT IGNORE INTO tweets (tweet_id, content)
 VALUES (1, 'Vote for Biden'),(2,'Let us make America great again!');
